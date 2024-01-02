4b7039704b96226a1adde57fb5c38af29bb8da42
Arun Vishwakarma <avishwakarma3009@gmail.com>
2024-01-02 19:37:22 +0530

    Updated

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express'); // it allow us to handle HTTP request 
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth_routes');
-const dotenv = require('dotenv');
 
-dotenv.config();
 const app = express(); // instance of express
-const PORT = 3000;
+const PORT = 9000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World"
-)
+    res.send(
+        "Hello World"
+    )
 });
 
-mongoose.connect(process.env.MONGO_URL).then(
+mongoose.connect('mongodb+srv://arunv3009:arun30EasyRideDB@easyridedb.wbuq8wm.mongodb.net/EasyRide').then(
     () => (console.log("Database is connected😊"))
 ).catch(
     (err) => {
