f6fa09cb1d3e2f2b51496ed5ffd579350e09226f
Arun Vishwakarma <avishwakarma3009@gmail.com>
2024-01-02 19:55:57 +0530

    Dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express'); // it allow us to handle HTTP request 
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth_routes');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express(); // instance of express
-const PORT = 9000;
 
 app.get('/', (req, res) => {
     res.send(
@@ -11,7 +12,8 @@ app.get('/', (req, res) => {
     )
 });
 
-mongoose.connect('mongodb+srv://arunv3009:arun30EasyRideDB@easyridedb.wbuq8wm.mongodb.net/EasyRide').then(
+
+mongoose.connect(process.env.MONGO_URL).then(
     () => (console.log("Database is connected😊"))
 ).catch(
     (err) => {
@@ -22,7 +24,7 @@ mongoose.connect('mongodb+srv://arunv3009:arun30EasyRideDB@easyridedb.wbuq8wm.mo
 app.use(express.json());
 app.use('/api/', authRoute);
 
-app.listen(PORT,
-    console.log(`App is running at PORT ${PORT}`)
+app.listen(process.env.PORT || 50001,
+    console.log(`App is running at PORT ${process.env.PORT || 5001}`)
 );
 
